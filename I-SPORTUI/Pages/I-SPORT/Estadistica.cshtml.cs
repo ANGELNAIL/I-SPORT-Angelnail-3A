149ed90a82320d53edef5c49f88362c5302ec80d
ANGELNAIL <angelnail@outlook.es>
2020-08-07 21:36:43 -0500

    Reportes individuales y generales

--- I-SPORTUI/Pages/I-SPORT/Estadistica.cshtml.cs
+++ I-SPORTUI/Pages/I-SPORT/Estadistica.cshtml.cs
@@ -14,30 +14,42 @@ namespace I_SPORTUI.Pages.I_SPORT
         public readonly IRepository<Jugador> jrepository;
         public readonly IRepository<Estadisticas> Repository;
         public readonly IRepository<Partido> prepository;
+        public readonly IRepository<Equipo> erepository;
+        public  IRepository<paises> parepository;
         public IEnumerable<Jugador> Jugadores { get; set; }
         public IEnumerable<Estadisticas> Estadisticas { get; set; }
         public IEnumerable<Partido> partidos { get; set; }
+        public IEnumerable<Equipo> equipos { get; set; }
+        public IEnumerable<paises> paises { get; set; }
         public Jugador jugador { get; private set; }
         public Estadisticas estadisticas { get; private set; }
         public Partido partido { get; private set; }
+        public Equipo equipo { get; set; }
+        public paises pais { get; set; }
         public int tgoles { get; set; }
         public int trojas { get; set; }
         public int tamarillas { get; set; }
         public int tjugado { get; set; }
         public int tautogoles { get; set; }
 
-        public EstadisticaModel (IRepository<Jugador> jrepository, IRepository<Estadisticas> repository, IRepository<Partido> prepository)
+        public EstadisticaModel (IRepository<Jugador> jrepository, IRepository<Estadisticas> repository, IRepository<Partido> prepository, IRepository<Equipo> erepository, IRepository<paises> parepository)
         {
             this.Repository = repository;
             this.jrepository = jrepository;
             this.prepository = prepository;
+            this.erepository = erepository;
+            this.parepository = parepository;
         }
         public IActionResult OnGet(int Id)
         {
             
-            Estadisticas = Repository.Get2(Id);            
+            Estadisticas = Repository.Get2(Id);
+            jugador = jrepository.Get(Id);
+            equipo = erepository.Get(Id);
+            pais = parepository.Get(Id);
             Jugadores = jrepository.GetAll();
             partidos = prepository.GetAll();
+            equipos = erepository.GetAll();
             tautogoles = Repository.Getsumag(Id);
             tamarillas = Repository.Getsumta(Id);
             trojas = Repository.Getsumtr(Id);
